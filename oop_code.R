f5bb49a168d6f2ffae84df6dc85b59cd0b3ca0d1
476458 <476458@LT032871>
2017-06-08 14:56:45 +0530

    Refactored seriously

--- oop_code.R
+++ oop_code.R
@@ -161,9 +161,6 @@ LongitudinalData <- setRefClass("LongitudinalData",
 				getmeasurementslength = function(){
 					length(measurements)
 				},
-				getsubjectid = function( sub ){
-					subject( sub )
-				},
 				findsubject = function( id ){
 					result <- 'NA'
 					measurements %>% map(., function(x) {
@@ -226,81 +223,19 @@ LongitudinalData <- setRefClass("LongitudinalData",
 					summaries %>% summary
 				}
 				},subjectsummary = function( subject ){
-					summation <- vector(mode="list", length=18)
-					for(i in 1:18)
-					{
-						summation[[i]] <- 0
-					}  
-					names(summation) <- c("visit0bedroom", "visit0livingroom", "visit0tvroom", "visit0den", "visit0room", "visit0diningroom",
-								          "visit1bedroom", "visit1livingroom", "visit1tvroom", "visit1den", "visit1room", "visit1diningroom",
-										  "visit2bedroom", "visit2livingroom", "visit2tvroom", "visit2den", "visit2room", "visit2diningroom")
-					if(is.character(subject) && subject == 'NA'){
-						subject
-					}else{
-						summarydata <- subsummary(subject)
-						measurements %>% map(., function(x) {
-									if (x$getid() == subject$getid() ){
-										m <<- x$getmeasurement()
-										if( m$getvisit()  == 0){
-											if( x$getlocation()$getlocation() == 'bedroom'){
-												summation$visit0bedroom <<- summation$visit0bedroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'living room'){
-												summation$visit0livingroom <- summation$visit0livingroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'tv room'){
-												summation$visit0tvroom <- summation$visit0tvroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'den'){
-												summation$visit0den <- summation$visit0den  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'dining room'){
-												summation$visit0diningroom <- summation$visit0diningroom  + m$getquantity()$amount
-											}
-										}else
-										if( m$getvisit()  == 1){
-											if( x$getlocation()$getlocation() == 'bedroom'){
-												summation$visit1bedroom <- summation$visit1bedroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'living room'){
-												summation$visit1livingroom <- summation$visit1livingroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'tv room'){
-												summation$visit1tvroom <- summation$visit1tvroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'den'){
-												summation$visit1den <- summation$visit1den  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'dining room'){
-												summation$visit1diningroom <- summation$visit1diningroom  + m$getquantity()$amount
-											}
-										}else
-										if( m$getvisit()  == 2){
-											if( x$getlocation()$getlocation() == 'bedroom'){
-												summation$visit2bedroom <- summation$visit2bedroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'living room'){
-												summation$visit2livingroom <- summation$visit2livingroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'tv room'){
-												summation$visit2tvroom <- summation$visit2tvroom  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'den'){
-												summation$visit2den <- summation$visit2den  + m$getquantity()$amount
-											}else
-											if( x$getlocation()$getlocation() == 'dining room'){
-												summation$visit2diningroom <- summation$visit2diningroom  + m$getquantity()$amount
-											}
-										}
-									}
+					filteredmeasurements <-
+					keep(measurements, function(x){
+								x$getid() == subject$getid()
 								})
-						
-					finaldata <-  sapply( append(uniquelocations(measurements), list('visit'), 0),paste, USE.NAMES=F)
-					finaldata <-paste(finaldata, sep="\n")
-					finaldata <-paste(cat(finaldata,"\n"),cat(paste('  0      ')),cat(paste(summation$visit0bedroom, sep="\n")))
-					finaldata <-paste(cat(finaldata,"\n"),cat(paste('  1      ', sep="\n")))
-					finaldata <-paste(cat(finaldata,"\n"),cat(paste('  2      ', sep="\n")))
-					}
+					groupedmeasurements <-
+						filteredmeasurements %>% lapply(function(x){
+								m <<- x$getmeasurement()
+								as.data.frame(list('visit'=m$getvisit(),
+												    'location'=x$getlocation()$getlocation(),
+													'amount'=m$getquantity()$amount))
+							}) %>% rbind_all() 
+					dataColumns <- c('amount')
+					ddply(groupedmeasurements,c('visit','location'),function(x) colSums(x[dataColumns]))
 				}
 					
 		) 
